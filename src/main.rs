95dc657d9c84dceb58f8363b9a628582352b7e82
Alejandro Salas Ledezma <alejandro.ledezma16@gmail.com>
2023-01-08 13:36:46 -0400

    add functionality to read a specified file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +7,9 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {} in file {}", query, file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Error while trying to read the file :(");
+
+    println!("With text:\n{contents}");
 }
