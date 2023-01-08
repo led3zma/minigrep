26396f58adf0308f623a115ccd84489c2f77f659
Alejandro Salas Ledezma <alejandro.ledezma16@gmail.com>
2023-01-08 15:19:57 -0400

    Add search functionality with TDD

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {err}");
         process::exit(1);
     });
 
-    println!(
-        "Searching for \"{}\" in file \"{}\"",
-        config.query, config.file_path
-    );
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error!: {e}");
         process::exit(1);
     }
 }
